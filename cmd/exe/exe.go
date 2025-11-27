04854bb2e08fe274b781c13987efb59aac8f7459
stefan <stefantomazic8923@gmail.com>
2025-11-27 16:25:31 +0100

    narejena paketa main in redovalnica

--- cmd/exe/exe.go
+++ cmd/exe/exe.go
@@ -1,1 +1,43 @@
-package exe
+package main
+
+import (
+	f "fmt"
+
+	"github.com/stfntmzc/ps_dn5/pkg/redovalnica"
+)
+
+func main() {
+
+	studenti := map[string]redovalnica.Student{
+		"01010101": {Ime: "Adam", Priimek: "Bohorič", Ocene: []int{10, 10, 10, 10, 9}},
+		"12121212": {Ime: "Bine", Priimek: "Cvetković", Ocene: []int{8, 9, 9, 8, 9}},
+		"23232323": {Ime: "Jan", Priimek: "Beržan", Ocene: []int{8, 9, 9, 9, 9}},
+		"34343434": {Ime: "Andraž", Priimek: "Novinšek", Ocene: []int{6, 10, 9, 8, 7}},
+		"45454545": {Ime: "Janez", Priimek: "Novak", Ocene: []int{6, 6, 5, 5, 5}},
+		"67676767": {Ime: "Jurij", Priimek: "Dalmatin", Ocene: []int{9, 9, 9, 9}},
+		"78787878": {Ime: "Primož", Priimek: "Trubar", Ocene: []int{6, 6, 6, 6, 6}},
+	}
+
+	f.Println("DodajOceno ===========================")
+	f.Println("")
+	f.Println("Dodajanje ocene Janu Beržanu:")
+	f.Println(studenti["23232323"])
+	redovalnica.DodajOceno(studenti, "23232323", 8)
+	f.Println(studenti["23232323"])
+	f.Println("")
+	f.Println("Dodajanje ocene študentu, ki ni na seznamu (vpisna številka 56565656):")
+	redovalnica.DodajOceno(studenti, "56565656", 8)
+	f.Println("")
+	f.Println("Dodajanje ocene, ki je izven intervala [1,10]:")
+	redovalnica.DodajOceno(studenti, "23232323", 23)
+	f.Println("")
+
+	f.Println("IzpisVsehOcen ===========================")
+	f.Println("")
+	redovalnica.IzpisVsehOcen(studenti)
+	f.Println("")
+
+	f.Println("IzpisiKoncniUspeh ===========================")
+	f.Println("")
+	redovalnica.IzpisiKoncniUspeh(studenti)
+}
--- pkg/redovalnica/redovalnica.go
+++ pkg/redovalnica/redovalnica.go
@@ -1,1 +1,68 @@
 package redovalnica
+
+import f "fmt"
+
+type Student struct {
+	Ime     string
+	Priimek string
+	Ocene   []int
+}
+
+func DodajOceno(studenti map[string]Student, vpisnaStevilka string, ocena int) {
+	if ocena < 1 || 10 < ocena {
+		f.Println("Ocena mora biti število od 1 do 10.")
+		return
+	}
+	_, obstaja := studenti[vpisnaStevilka]
+	if !obstaja {
+		f.Printf("Študenta z vpisno številko %s ni na seznamu.\n", vpisnaStevilka)
+		return
+	}
+
+	student := studenti[vpisnaStevilka]
+	student.Ocene = append(student.Ocene, ocena)
+	studenti[vpisnaStevilka] = student
+}
+
+func IzpisVsehOcen(studenti map[string]Student) {
+	f.Println("VSE OCENE:")
+	for vpisnaStevilka, student := range studenti {
+		f.Printf("%s - %s %s: [", vpisnaStevilka, student.Ime, student.Priimek)
+		for i, ocena := range student.Ocene {
+			f.Printf("%d", ocena)
+			if i < len(student.Ocene)-1 {
+				f.Printf(" ")
+			}
+		}
+		f.Printf("]\n")
+	}
+}
+
+func IzpisiKoncniUspeh(studenti map[string]Student) {
+	for vpisnaStevilka, student := range studenti {
+		avg := povprecje(studenti, vpisnaStevilka)
+		f.Printf("%s %s: povprečna ocena %.2f -> ", student.Ime, student.Priimek, povprecje(studenti, vpisnaStevilka))
+		switch {
+		case avg >= 9.0:
+			f.Println("Odličen študent!")
+		case avg >= 6.0:
+			f.Println("Povprečen študent")
+		default:
+			f.Println("Neuspešen študent")
+		}
+	}
+}
+
+func povprecje(studenti map[string]Student, vpisnaStevilka string) float64 {
+	_, obstaja := studenti[vpisnaStevilka]
+	if !obstaja {
+		return -1.0
+	}
+
+	var avg float64 = 0.0
+	for _, ocena := range studenti[vpisnaStevilka].Ocene {
+		avg += float64(ocena)
+	}
+
+	return (avg / float64(len(studenti[vpisnaStevilka].Ocene)))
+}
